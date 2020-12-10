286a68c565ef3db3f7d58fc6c7877c6345f8826d
Juan Becerra <juan.becerra.villar@gmail.com>
2020-12-10 17:04:40 -0300

    descripcion que quieras

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace proyecto_web_empresa_a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
